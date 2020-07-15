9a676d34448f9afafa53204499a5b11417b9d8d3
sodrooome <15523163@students.uii.ac.id>
2020-07-15 12:06:29 +0700

    add: expired time for caching object

--- lru/decorators.py
+++ lru/decorators.py
@@ -1,5 +1,6 @@
-from lru.heap import Heap
+import functools
 from lru.lrucache import LRUCache
+from datetime import datetime, timedelta
 
 def lru_cache(capacity: int=128, **kwargs) -> int:
     """Decorators for LRUCache classes. Given the
@@ -14,10 +15,40 @@ def lru_cache(capacity: int=128, **kwargs) -> int:
         return LRUCache(capacity=capacity, **kwargs)
     return wrapper
 
+def lru_cache_time(capacity: int=128, seconds: int=60*15, **kwargs) -> int:
+    """Decorators for LRUCache classes using
+    expired cached time. This is an mock only,
+    probably not ready to bump into major version. for example:
+
+    ```
+    @lru_cache_time(capacity=3, seconds=60)
+    def foo(x):
+        pass
+    ```
+    """
+    def wrapper(func):
+        update_time = timedelta(seconds=seconds)
+        next_update_time = datetime.utcnow() + update_time
+        now_time = datetime.utcnow()
+        func = LRUCache(capacity=capacity, seconds=seconds, **kwargs)
+        return func
+
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            # using nonlocal for defined
+            # variable inside nested function
+            nonlocal next_update_time, func
+            if now_time > next_update_time:
+                func.clear_all()
+                next_update_time = now_time + update_time
+            return func(*args, **kwargs)
+        return wrapped
+    return wrapper
+
 
 # mock test
 """
-@lru_cache(capacity=5)
+@lru_cache_time(capacity=5, seconds=60)
 def test_lru(x):
     print("Calling f(" + str(x) + ")")
     return x
@@ -28,4 +59,6 @@ test_lru.set(3, "foos")
 test_lru.set(4, "fc")
 test_lru.set(5, "set")
 print(test_lru.get_capacity())
+print(test_lru.get_dict())
+print(test_lru.clear_all())
 """
