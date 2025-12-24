ff45926824cfd5b82e954a47fe231efc0cdba941
Ryan Febriansyah <ryanfeb@ajobthing.com>
2025-12-24 11:19:40 +0700

    patch unittest for lrucache modules due to the recent bugfixes

--- tests/test_lrucache.py
+++ tests/test_lrucache.py
@@ -24,20 +24,21 @@ class LRUCacheTest(unittest.TestCase):
 
     def test_get_cache_dict(self):
         self.assertTrue(self.testLRU.get_dict())
+
+    def test_check_whether_cache_is_empty(self):
+        self.testLRU.clear_all()
+        self.assertTrue(self.testLRU.is_empty())
     
     def test_get_cache_duration(self):
         self.assertTrue(self.testLRU.get_duration(expired_time=3600))
-        # this should be return into False
-        # cause the maximum duration
-        # is 3600 seconds
-        # todo: Fix this test
-        self.assertTrue(self.testLRU.get_duration(expired_time=4800))
+        self.assertFalse(self.testLRU.get_duration(expired_time=100))
 
     def test_get_least_recently_used(self):
         self.assertTrue(self.testLRU.get_lru_element())
 
     def test_get_ttl(self):
         self.assertTrue(self.testLRU.get_ttl(1))
+        self.assertFalse(self.testLRU.get_ttl(key=5))
 
     def test_cache_is_empty(self):
         self.assertFalse(self.testLRU.is_empty())
@@ -49,7 +50,10 @@ class LRUCacheTest(unittest.TestCase):
         self.assertIsNone(self.testLRU.clear_cache_key(1))
 
     def test_get_capacity(self):
-        self.assertFalse(self.testLRU.get_capacity())
+        self.assertTrue(self.testLRU.get_capacity())
+
+    def test_get_time_to_live(self):
+        self.assertEqual(self.testLRU.ttl, 900)
 
     def test_string_output_for_cache(self):
         self.assertTrue(self.testLRU.__str__())
@@ -59,6 +63,9 @@ class LRUCacheTest(unittest.TestCase):
 
     def test_scale_capacity(self):
         self.assertTrue(self.testLRU(capacity=300))
+
+    def test_cache_length_method(self):
+        self.assertEqual(self.testLRU.__len__(), 3)
 
     def test_get_key_update_access_time(self):
         # mocking time for testing purposes, this test
@@ -106,6 +113,10 @@ class LRUCacheTestInitialization(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             LRUCache(seconds=60*15*15*15)
+
+        with self.assertRaises(KeyError):
+            cache = LRUCache(capacity=1)
+            cache.get(key=1)
 
     def test_equality_for_cache_dict(self):
         cache_1 = LRUCache(capacity=3)
